89369426688af3627574c9af6566b1d5fdcc56c7
U-LB\chanwit <chanwit@gmail.com>
2009-03-18 02:39:49 +0700

    implemented create-zul

--- scripts/CreateZul.groovy
+++ scripts/CreateZul.groovy
@@ -1,20 +1,46 @@
+/*
+ * Copyright 2004-2005 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Gant script that creates a new Grails controller
+ * 
+ * @author Chanwit
+ *
+ * @since 0.7
+ */
+
+import grails.util.GrailsNameUtils
+
 includeTargets << grailsScript("_GrailsInit")
 includeTargets << grailsScript("_GrailsCreateArtifacts")
 
-target(main: "The description of the script goes here!") {
+target ('default': "Creates a new zul page") {
     depends(checkVersion, parseArguments)
 
-    def type = "Controller"
+    def type = "ZUL"
     promptForName(type: type)
+    def name = argsMap["params"][0]    
+    def propertyName = GrailsNameUtils.getPropertyNameRepresentation(name)      
+    def zulFile = "${basedir}/web-app/${propertyName}.zul"
+    
+    ant.copy(file:"${zkPluginDir}/src/templates/artifacts/template.zul",
+             tofile: zulFile,
+             overwrite: true)             
+    ant.replace(file: zulFile, token: "@artifact.name@", value: propertyName)
 
-    def name = argsMap["params"][0]
-	createArtifact(name: name, suffix: type, type: type, path: "web-inf/")
-
-    def viewsDir = "${basedir}/grails-app/views/${propertyName}"
-    ant.mkdir(dir:viewsDir)
-	event("CreatedFile", [viewsDir])
-
-	// createUnitTest(name: name, suffix: type, superClass: "ControllerUnitTestCase")
+    // followed by creating composer for it    
+	createArtifact(name: name, suffix: "Composer", type: "Composer", path: "grails-app/composers")
 }
-
-setDefaultTarget(main)
