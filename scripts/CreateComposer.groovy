6f4c45f6b451c8923862d56ee627777c6372da57
U-LB\chanwit <chanwit@gmail.com>
2009-03-31 00:06:56 +0100

    support generating facade in a composer

--- scripts/CreateComposer.groovy
+++ scripts/CreateComposer.groovy
@@ -1,12 +1,12 @@
 /*
  * Copyright 2004-2005 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,11 +16,13 @@
 
 /**
  * Gant script that creates a new Grails controller
- * 
+ *
  * @author Chanwit
  *
- * @since 0.4
+ * @since 0.7.1
  */
+
+import grails.util.GrailsNameUtils
 
 includeTargets << grailsScript("_GrailsInit")
 includeTargets << grailsScript("_GrailsCreateArtifacts")
@@ -32,11 +34,16 @@ target ('default': "Creates a new composer") {
     promptForName(type: type)
 
     def name = argsMap["params"][0]
-	createArtifact(name: name, suffix: type, type: type, path: "grails-app/composers")
+    def propName = GrailsNameUtils.getPropertyNameRepresentation(name)
+    def filename = GrailsNameUtils.getClassName(name, type)
 
-    // def viewsDir = "${basedir}/grails-app/views/${propertyName}"
-    // ant.mkdir(dir:viewsDir)
-	// event("CreatedFile", [viewsDir])
+    createArtifact(name: name, suffix: type, type: type, path: "grails-app/composers")
 
-	// createUnitTest(name: name, suffix: type, superClass: "ControllerUnitTestCase")
+    // create a facade property for the composer
+    ant.replace(
+        file: "${basedir}/grails-app/composers/${filename}.groovy",
+        token: "@artifact.name.prop@",
+        value: propName
+    )
+
 }
--- scripts/CreateZul.groovy
+++ scripts/CreateZul.groovy
@@ -1,12 +1,12 @@
 /*
  * Copyright 2004-2005 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,7 +16,7 @@
 
 /**
  * Gant script that creates a new Grails controller
- * 
+ *
  * @author Chanwit
  *
  * @since 0.7
@@ -32,15 +32,31 @@ target ('default': "Creates a new zul page") {
 
     def type = "ZUL"
     promptForName(type: type)
-    def name = argsMap["params"][0]    
-    def propertyName = GrailsNameUtils.getPropertyNameRepresentation(name)      
-    def zulFile = "${basedir}/web-app/${propertyName}.zul"
-    
-    ant.copy(file:"${zkPluginDir}/src/templates/artifacts/template.zul",
-             tofile: zulFile,
-             overwrite: true)             
-    ant.replace(file: zulFile, token: "@artifact.name@", value: propertyName)
+    def name = argsMap["params"][0]
+    def propName = GrailsNameUtils.getPropertyNameRepresentation(name)
+    def zulFile = "${basedir}/web-app/${propName}.zul"
 
-    // followed by creating composer for it    
-	createArtifact(name: name, suffix: "Composer", type: "Composer", path: "grails-app/composers")
+    ant.copy(
+        file:"${zkPluginDir}/src/templates/artifacts/template.zul",
+        tofile: zulFile,
+        overwrite: true
+    )
+
+    ant.replace(
+        file: zulFile,
+        token: "@artifact.name@",
+        value: propName
+    )
+
+    // following by creating composer for it
+    createArtifact(name: name, suffix: "Composer", type: "Composer", path: "grails-app/composers")
+
+    // create a facade property for the composer
+    def filename = GrailsNameUtils.getClassName(name, "Composer")
+    ant.replace(
+        file: "${basedir}/grails-app/composers/${filename}.groovy",
+        token: "@artifact.name.prop@",
+        value: propName
+    )
+
 }
