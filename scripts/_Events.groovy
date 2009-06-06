fca85208fc46e12d8cb541876dd2f1b695aeac2a
U-LB\chanwit <chanwit@gmail.com>
2009-06-06 17:19:20 +0100

    added event for plugin-load-end

---
+++ scripts/_Events.groovy
@@ -0,0 +1,29 @@
+
+eventPluginLoadEnd = { pluginManager ->
+    println "Copy theme jars to ZK plugin directory"
+    ant.copy(todir:"${zkPluginDir}/lib/", overwrite: true) {
+        fileset(dir:"${basedir}/zk-themes/", includes:"*.jar")
+    }
+}
+
+eventCreateWarStart = { warLocation, stagingDir ->
+    def appVersion = metadata.'app.version'
+    def appName = config.google.appengine.application ?: grailsAppName
+
+    println "Generating ZK support appengine-web.xml file for application [$appName]"
+    new File("$stagingDir/WEB-INF/appengine-web.xml").write """<?xml version=\"1.0\" encoding=\"utf-8\"?>
+<appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\">
+    <application>${appName}</application>
+    <version>${appVersion}</version>
+    <sessions-enabled>true</sessions-enabled>
+    <static-files>
+        <exclude path=\"/**.zul\"/>
+        <exclude path=\"/**.zhtml\"/>
+    </static-files>
+    <resource-files>
+        <include path=\"/**.zul\"/>
+        <include path=\"/**.zhtml\"/>
+    </resource-files>
+</appengine-web-app>
+"""
+}
