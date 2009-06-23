66e858a7c93bc307fda012f15239c582185d56dc
chanwit <chanwit@gmail.com>
2009-06-23 16:14:42 +0100

    changed version to 0.7.4

--- ZkGrailsPlugin.groovy
+++ ZkGrailsPlugin.groovy
@@ -4,7 +4,7 @@ import org.zkoss.zkplus.databind.BindingListModelList
 
 class ZkGrailsPlugin {
     // the plugin version
-    def version = "0.7.3"
+    def version = "0.7.4"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.1 > *"
     // the other plugins this plugin depends on
