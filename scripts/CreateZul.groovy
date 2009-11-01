fce64c6953030868be5c1a8c45b301212de6eb72
Chanwit Kaewkasi <chanwit@gmail.com>
2009-11-01 21:44:06 +0700

    corrected javadoc and copyright

--- scripts/CreateZul.groovy
+++ scripts/CreateZul.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2005 the original author or authors.
+ * Copyright 2007-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,9 +15,9 @@
  */
 
 /**
- * Gant script that creates a new Grails controller
+ * Gant script that creates a new ZUL page with an associated Grails composer
  *
- * @author Chanwit
+ * @author Chanwit Kaewkasi
  *
  * @since 0.7
  */
