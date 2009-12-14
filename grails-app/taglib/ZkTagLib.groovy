17a0889ce32741d585351283c01959cba7784cc8
Gustavo Madruga <omadruga@gmail.com>
2009-12-15 01:51:44 +0800

    implemented #77 - hability to load zk i3-label from gsp via taglib

--- grails-app/taglib/ZkTagLib.groovy
+++ grails-app/taglib/ZkTagLib.groovy
@@ -1,4 +1,5 @@
 import org.zkoss.zkgrails.ZulResponse
+import org.zkoss.util.resource.Labels
 
 class ZkTagLib {
 
@@ -12,6 +13,51 @@ class ZkTagLib {
     def body = { attrs, b ->
         cacheZul(attrs['zul'])
         out << pageScope.model['body']
+    }
+
+    def l = { attrs, b ->
+        String key = attrs["key"] as String
+        List args = attrs["args"] as List
+        Boolean required = attrs["required"] as Boolean
+        String defValue = attrs["default"] as String
+        def label
+
+        if(!key) {
+            throwTagError("Attribute [key] is obrigatory")
+        }
+        if(required != null && !(required instanceof Boolean)) {
+            throwTagError("Attribute [required] has to be a boolean value [true|false]")            
+        }
+        if(args && !(args instanceof List)) {
+            throwTagError("Attribute [args] has to be a list")
+        }
+        if(defValue && !(defValue instanceof String)) {
+            throwTagError("Attribute [default] has to be a String")            
+        }
+
+        if(! required) {
+            if(! defValue) {
+                if(! args) {
+                    label = Labels.getLabel(key)
+                } else {
+                    label = Labels.getLabel(key, args as Object[])
+                }
+            } else {
+                if(args) {
+                    label = Labels.getLabel(key, defValue, args as Object[])
+                } else {
+                    label = Labels.getLabel(key, defValue)
+                }
+            }
+        } else {
+            if(! args) {
+                label = Labels.getRequiredLabel(key)
+            } else {
+                label = Labels.getRequiredLabel(key, args as Object[])
+            }
+        }
+
+        out << label
     }
 
     private cacheZul(url) {            
