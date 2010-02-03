39a0950659d668dce2783a8b1007295d99d2b376
Chanwit Kaewkasi <chanwit@gmail.com>
2010-02-03 15:35:56 +0000

    Issue 85: taglibs are now working with ZUL.

--- src/java/org/zkoss/web/util/resource/CustomContentLoader.java
+++ src/java/org/zkoss/web/util/resource/CustomContentLoader.java
@@ -2,38 +2,59 @@ package org.zkoss.web.util.resource;
 
 import javax.servlet.ServletContext;
 import org.zkoss.io.Files;
-import java.io.File;
-import java.io.InputStream;
-import java.io.FileInputStream;
-import java.io.BufferedInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 import java.net.URL;
-
 import org.zkoss.util.resource.Locator;
 import org.zkoss.zk.ui.metainfo.Parser;
 import org.zkoss.zk.ui.metainfo.PageDefinitions;
 import org.zkoss.zk.ui.WebApp;
 
+import org.codehaus.groovy.grails.web.pages.*;
+
+import groovy.text.Template;
+import groovy.lang.Writable;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.core.io.FileSystemResource;
+import org.zkoss.zk.ui.metainfo.PageDefinition;
+
 public class CustomContentLoader extends ResourceLoader {
 
 	private final WebApp _wapp;
+	private final ApplicationContext _ctx;
 
 	public CustomContentLoader(WebApp wapp) {
 		_wapp = wapp;
+		_ctx  = WebApplicationContextUtils.getRequiredWebApplicationContext(
+			(ServletContext)wapp.getNativeContext()
+		);
 	}
 
 	//-- super --//
 	protected Object parse(String path, File file, Object extra)
 	throws Exception {
-		System.out.println("parsing file");
-		final Locator locator =
-			extra != null ? (Locator)extra: PageDefinitions.getLocator(_wapp, path);
-		return new Parser(_wapp, locator).parse(file, path);
+		Locator locator = null;
+		if(extra !=null)
+			locator = (Locator)extra;
+		else
+			locator = PageDefinitions.getLocator(_wapp, path);
+
+		GroovyPagesTemplateEngine gsp = (GroovyPagesTemplateEngine)_ctx.getBean("groovyPagesTemplateEngine");
+		Template template = gsp.createTemplate(new FileSystemResource(file));
+		HashMap model = new HashMap();
+		Writable w = template.make(model);
+		StringWriter sw = new StringWriter();
+		w.writeTo(new PrintWriter(sw));
+		StringReader reader = new StringReader(sw.toString());        
+		PageDefinition pgdef = new Parser(_wapp, locator).parse(reader, null);
+		pgdef.setRequestPath(path);
+		return pgdef;
 	}
 
 	protected Object parse(String path, URL url, Object extra)
 	throws Exception {
-		System.out.println("parsing url");
 		final Locator locator =
 			extra != null ? (Locator)extra: PageDefinitions.getLocator(_wapp, path);
 		return new Parser(_wapp, locator).parse(url, path);		
@@ -43,43 +64,3 @@ public class CustomContentLoader extends ResourceLoader {
 		PageDefinitions.setResourceLoaderClass(CustomContentLoader.class);
 	}
 }
-
-/*
-public class CustomContentLoader extends ResourceLoader {
-
-		private final ServletContext _ctx;
-		private CustomContentLoader(ServletContext ctx) {
-			_ctx = ctx;
-		}
-
-		//-- super --//
-		protected Object parse(String path, File file, Object extra)
-		throws Exception {
-			System.out.println("parse0 by CustomContentLoader");
-			final InputStream is = new BufferedInputStream(new FileInputStream(file));
-			try {
-				return readAll(is);
-			} finally {
-				Files.close(is);
-			}
-		}
-
-		protected Object parse(String path, URL url, Object extra)
-		throws Exception {
-			System.out.println("parse1 by CustomContentLoader");
-			InputStream is = url.openStream();
-			if (is != null) is = new BufferedInputStream(is);
-			try {
-				return readAll(is);
-			} finally {
-				Files.close(is);
-			}
-		}
-
-		private String readAll(InputStream is) throws Exception {
-			System.out.println("readAll by CustomContentLoader");
-			if (is == null) return null;
-			return Files.readAll(new InputStreamReader(is, "UTF-8")).toString();
-		}
-	}
-*/
