c70ecfd3c9ef3511e7a00196cf862e117d85c69d
Chanwit Kaewkasi <chanwit@gmail.com>
2010-02-03 17:14:28 +0000

    fixed template-hashmap scope

--- src/java/org/zkoss/zkgrails/TemplateHashMap.java
+++ src/java/org/zkoss/zkgrails/TemplateHashMap.java
@@ -2,7 +2,7 @@ package org.zkoss.zkgrails;
 
 import java.util.HashMap;
 
-class TemplateHashMap extends HashMap {
+public class TemplateHashMap extends HashMap {
     
     @Override
     public Object get(Object key) {
