9bd27c97b271040c16da8d2d0d2ffd22efcebf19
Chanwit Kaewkasi <chanwit@gmail.com>
2010-05-12 12:48:03 +0700

    fixed regression for issue 125

--- test/selenium/zk/Issue125Tests.groovy
+++ test/selenium/zk/Issue125Tests.groovy
@@ -13,10 +13,6 @@ class Issue125Tests extends GroovyTestCase {
             open "/zk/issue_125_test_comet.zul"
             assertEquals "Ready", getText("zk_comp_3")
             click "zk_comp_2"
-            waitForText("zk_comp_3", "time : 1")
-            waitForText("zk_comp_3", "time : 2")
-            waitForText("zk_comp_3", "time : 3")
-            waitForText("zk_comp_3", "time : 4")
             waitForText("zk_comp_3", "time : 5")
             assertEquals "time : 5", getText("zk_comp_3")
         }
