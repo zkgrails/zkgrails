76a4332bebce05b917b9c69c1fe52d649454fcc2
Chanwit Kaewkasi <chanwit@gmail.com>
2010-05-30 09:25:07 +0700

    changed version to M5

--- ZkGrailsPlugin.groovy
+++ ZkGrailsPlugin.groovy
@@ -6,7 +6,7 @@ import org.zkoss.zkgrails.scaffolding.DefaultScaffoldingTemplate
 
 class ZkGrailsPlugin {
     // the plugin version
-    def version = "1.0-M4"
+    def version = "1.0-M5"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.1 > *"
     // the other plugins this plugin depends on
