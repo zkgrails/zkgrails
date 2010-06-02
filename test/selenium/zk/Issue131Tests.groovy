b7691ebcf0776d8d042a1660b89fe43aeb4c0a36
Chanwit Kaewkasi <chanwit@gmail.com>
2010-06-02 14:31:03 +0700

    test case for issue 131

---
+++ test/selenium/zk/Issue131Tests.groovy
@@ -0,0 +1,22 @@
+package zk
+
+import grails.test.*
+import com.thoughtworks.selenium.*
+import grails.plugins.selenium.SeleniumAware
+
+@Mixin(SeleniumAware)
+class Issue131Tests extends GroovyTestCase {
+
+    void testChineseDisplay() {
+        selenium.with {
+            setSpeed "1000"
+            open "/zk/issue_131.zul"
+            assertEquals "ทดสอบข้อความ", getText("zk_comp_2")
+            click "zk_comp_2"
+            assertEquals "คลิ๊ก", getText("zk_comp_2")
+            click "zk_comp_3"
+            assertEquals "คลิ๊ก test", getText("zk_comp_3")
+        }
+    }
+
+}
