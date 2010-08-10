49d5609b14dbe855e06189dd74ca7b13fa933b94
Madruga <omadruga@gmail.com>
2010-08-10 14:15:21 -0300

    fixes #139 - handling more than one comet per desktop. Also added more comets to test with

--- grails-app/comets/zk/Issue139_1Comet.groovy
+++ grails-app/comets/zk/Issue139_1Comet.groovy
@@ -8,11 +8,21 @@ class Issue139_1Comet extends GrailsComet {
 
     static trigger = [startDelay: 800L, every: 1000L]
 
+    def beforeExecute = { desktop, page ->
+        lblTest1.value = "begin"
+    }
+
     def execute = { desktop, page ->
         i++
         lblTest1.value = "time1 : ${i}"
 
-        if(i == 5) stop()
+        if(i == 5) {
+            i = 0
+            stop()
+        }
     }
 
+    def afterExecute = { desktop, page ->
+        lblTest1.value = "end"
+    }
 }
--- grails-app/comets/zk/Issue139_5Comet.groovy
+++ grails-app/comets/zk/Issue139_5Comet.groovy
@@ -1,20 +1,20 @@
 package zk
 
-import org.zkoss.zkgrails.*
+import org.zkoss.zkgrails.GrailsComet
 
-class Issue139_3Comet extends GrailsComet {
+class Issue139_5Comet extends GrailsComet {
 
     int i = 0
 
-    static trigger = [startDelay: 800L, every: 10000L]
+    static trigger = [startDelay: 10000L, every: 5000L]
 
     def beforeExecute = { desktop, page ->
-        lblTest3.value = "begin"
+        lblTest5.value = "begin"
     }
 
     def execute = { desktop, page ->
         i++
-        lblTest3.value = "time3 : ${i}"
+        lblTest5.value = "time5 : ${i}"
 
         if(i == 5) {
             i = 0
@@ -23,6 +23,6 @@ class Issue139_3Comet extends GrailsComet {
     }
 
     def afterExecute = { desktop, page ->
-        lblTest3.value = "end"
+        lblTest5.value = "end"
     }
 }
