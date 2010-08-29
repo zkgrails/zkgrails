84d5d2a5fdbc3f2d46876c365c82665026ae572f
Chanwit Kaewkasi <chanwit@gmail.com>
2010-08-29 23:27:43 +0700

    fixed issue 151 - enhanced message to support GORM errors

--- src/java/org/zkoss/zkgrails/MessageHolder.java
+++ src/java/org/zkoss/zkgrails/MessageHolder.java
@@ -7,9 +7,15 @@ import javax.swing.event.ListSelectionEvent;
 import org.codehaus.groovy.runtime.typehandling.DefaultTypeTransformation;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.context.MessageSourceResolvable;
+
+import org.springframework.validation.ObjectError;
+
 import org.springframework.web.context.support.WebApplicationContextUtils;
 import org.springframework.web.servlet.support.RequestContextUtils;
 import org.springframework.beans.factory.BeanCreationException;
+
 import org.zkoss.zk.ui.Page;
 
 import java.util.*;
@@ -30,7 +36,7 @@ public class MessageHolder {
         }
     }
 
-    String getAt(String code) {
+    public String getAt(String code) {
         return messageSource.getMessage(
                     code,
                     null,
@@ -38,14 +44,27 @@ public class MessageHolder {
                );
     }
 
-    String call(Map map) {
+    public String call(Map map) {
+
+        ObjectError error = (ObjectError)map.get("error");
         String code = (String)map.get("code");
         List args   = (List)map.get("args");
-        Object[] a = (Object[]) args.toArray(new Object[args.size()]);
-        return messageSource.getMessage(
-                    code,
-                    a,
-                    RequestContextUtils.getLocale(request)
-               );
+
+        if(error != null) {
+            String text;
+            try {
+                text = messageSource.getMessage(error, RequestContextUtils.getLocale(request) );
+            } catch (NoSuchMessageException e) {
+                if (error instanceof MessageSourceResolvable) {
+                    text = error.getCode();
+                } else {
+                    text = error.toString();
+                }
+            }
+            return text;
+        } else {
+            Object[] a = (Object[]) args.toArray(new Object[args.size()]);
+            return  messageSource.getMessage(code, a, RequestContextUtils.getLocale(request));
+        }
     }
 }
