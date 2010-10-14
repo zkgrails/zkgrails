cf8f51910bce05b47c3e70dae877e3488d00dffe
Chanwit Kaewkasi <chanwit@gmail.com>
2010-10-15 01:56:08 +0700

    back to firefox for headless hudson test.

--- grails-app/conf/SeleniumConfig.groovy
+++ grails-app/conf/SeleniumConfig.groovy
@@ -1,7 +1,7 @@
 selenium {
 	slow = true									// true to run tests in slow resources mode
 	singleWindow = true								// true for single window mode, false for multi-window mode
-	browser = "*googlechrome"							// can include full path to executable, default value is *firefox or *iexplore on Windows
+	browser = "*firefox"							// can include full path to executable, default value is *firefox or *iexplore on Windows
 	url = null										// the base URL for tests, defaults to Grails server url
     defaultTimeout = 30000  						// the timeout after which selenium commands will fail
 	windowMaximize = false  						// true to maximize browser on startup
