f9304b0f3c562b4826a0ec17fb03b743f1055eb1
Chanwit Kaewkasi <chanwit@gmail.com>
2010-10-16 15:03:10 +0700

    changed selenium timeout to 80000.

--- grails-app/conf/SeleniumConfig.groovy
+++ grails-app/conf/SeleniumConfig.groovy
@@ -3,7 +3,7 @@ selenium {
 	singleWindow = true								// true for single window mode, false for multi-window mode
 	browser = "*firefox"							// can include full path to executable, default value is *firefox or *iexplore on Windows
 	url = "http://localhost:8081/zk"				// the base URL for tests, defaults to Grails server url
-    defaultTimeout = 30000  						// the timeout after which selenium commands will fail
+    defaultTimeout = 80000  						// the timeout after which selenium commands will fail
 	windowMaximize = false  						// true to maximize browser on startup
 	screenshot {
 		dir = "./target/test-reports/screenshots"	// directory where screenshots are placed relative to project root
