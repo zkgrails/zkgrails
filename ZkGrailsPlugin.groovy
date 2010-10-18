1e98d411e3aaaf7fa7de9884acaf377c56867e90
Chanwit Kaewkasi <chanwit@gmail.com>
2010-10-19 01:27:26 +0700

    bumped version to 1.0-RC3.

--- ZkGrailsPlugin.groovy
+++ ZkGrailsPlugin.groovy
@@ -7,7 +7,7 @@ import grails.util.*
 
 class ZkGrailsPlugin {
     // the plugin version
-    def version = "1.0-RC2"
+    def version = "1.0-RC3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.1 > *"
     // the other plugins this plugin depends on
