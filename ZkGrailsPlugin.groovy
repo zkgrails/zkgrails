ea25557fa92b3ca5e0fb80b4a4b452389eb9d0ec
Chanwit Kaewkasi <chanwit@gmail.com>
2011-08-21 11:12:12 +0700

    dump version to 1.0.5

--- ZkGrailsPlugin.groovy
+++ ZkGrailsPlugin.groovy
@@ -7,7 +7,7 @@ import grails.util.*
 
 class ZkGrailsPlugin {
     // the plugin version
-    def version = "1.0.4"
+    def version = "1.0.5"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2 > *"
     // the other plugins this plugin depends on
