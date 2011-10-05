625b9030353a05e6f50423d5cac5b56565f9c3d2
Chanwit Kaewkasi <chanwit@gmail.com>
2011-10-05 13:01:28 +0700

    add ViewModel super class

---
+++ src/java/org/zkoss/zk/grails/GrailsViewModel.java
@@ -0,0 +1,40 @@
+package org.zkoss.zk.grails;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.zkoss.zkplus.databind.DataBinder;
+import groovy.lang.Closure;
+import org.zkoss.zk.grails.databind.BindingBuilder;
+import org.codehaus.groovy.grails.commons.GrailsClassUtils;
+
+public class GrailsViewModel implements InitializingBean {
+
+    private DataBinder binder = new DataBinder();
+
+    private String id;
+
+    public void setId(String value) { this.id = value; }
+    public String getId()           { return this.id;  }
+    
+    public DataBinder getBinder()   { return this.binder; }
+
+    @Override
+    public void afterPropertiesSet() {
+        //
+        binder.bindBean(this.getId(), this);
+        //
+        // get static binding
+        //        
+        Object obj = GrailsClassUtils.getPropertyOrStaticPropertyOrFieldValue(this, "binding");
+        if(obj instanceof Closure) {
+            Closure c = ((Closure)obj);
+            BindingBuilder bb = new BindingBuilder(this, this.getBinder());
+            c.setDelegate(bb);
+            c.setResolveStrategy(Closure.DELEGATE_FIRST);
+            c.call();
+        }
+        // invoke binding
+        //
+        // use binding object; How to obtain it from the composer?
+    }
+    
+}
