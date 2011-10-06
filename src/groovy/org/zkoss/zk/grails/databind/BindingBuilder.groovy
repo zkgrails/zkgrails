33e6eccd005c33954114d978a59645d33f3370bb
Chanwit Kaewkasi <chanwit@gmail.com>
2011-10-06 03:37:42 +0000

    deciding how to populate bean properties and match in the autowire mode

--- src/groovy/org/zkoss/zk/grails/databind/BindingBuilder.groovy
+++ src/groovy/org/zkoss/zk/grails/databind/BindingBuilder.groovy
@@ -32,7 +32,18 @@ class BindingBuilder {
         result.each { comp ->
             if(args instanceof Map) {
                 if(args.containsKey('autowire')) {
+                    //
                     // autowire mode
+                    //  suffix = match firstName <-> txtFirstName
+                    //  normal = match firstName <-> firstName
+                    //
+                    def autowire = args['autowire']
+                    def source   = args['source']
+                    def property = args['property']
+                    switch(autowire) {
+                        "normal": break// match each bean property with each comp?
+                        "suffix": break// match each bean property with each comp suffix?
+                    }
                 } else {
                     //
                     // normal mode
@@ -41,7 +52,7 @@ class BindingBuilder {
                     args.each { k, v -> 
                         this.binding.addBinding(comp, k, "${viewModel.id}.${v}")
                     }
-                }                
+                }
             }
         }
         this.binding.bindBean(this.viewModel.id, this.viewModel)
