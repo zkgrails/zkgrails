6c13496451711e7edcef5793e0bf674742e360f9
Chanwit Kaewkasi <chanwit@gmail.com>
2011-10-21 23:43:37 +0700

    refactor myViewModel to userViewModel

--- grails-app/domain/zk/User.groovy
+++ grails-app/domain/zk/User.groovy
@@ -3,7 +3,8 @@ package zk
 class User {
 
     String name
+    String lastName
 
-    String toString() { "${name}" }
+    String toString() { "${name} ${lastName}" }
 
 }
---
+++ grails-app/viewmodels/zk/UserViewModel.groovy
@@ -0,0 +1,24 @@
+package zk
+
+import org.zkoss.zk.grails.*
+
+class UserViewModel extends GrailsViewModel {
+
+    List<User> userList
+    User user
+
+
+    static binding = {
+        'button > *' autowire: byName, property: 'value'
+    }
+
+
+    //
+    // psuedo property
+    //
+    def fullname = [
+        forward: { "${user.name} ${user.lastName}" },    
+        reverse: { (user.name, user.lastName) = it.split(' ') }
+    ]
+
+}
