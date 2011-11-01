a4f33e2497ea407dbd219288543dc1a33ee33be5
Chanwit Kaewkasi <chanwit@gmail.com>
2011-11-01 16:38:19 +0700

    remove unused class

--- grails-app/viewmodels/zk/UserViewModel.groovy
+++ grails-app/viewmodels/zk/UserViewModel.groovy
@@ -30,5 +30,4 @@ class UserViewModel extends GrailsViewModel {
         else
             "color: black"
     }
-
 }
--- src/groovy/org/zkoss/zk/grails/databind/DependentObservable.groovy
+++
@@ -1,5 +0,0 @@
-package org.zkoss.zk.grails.databind
-
-class DependentObservable {
-
-}
