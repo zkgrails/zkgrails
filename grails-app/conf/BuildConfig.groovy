05956b5163b8ab05cc1a4bf59d23220673bb74cf
Chanwit Kaewkasi <chanwit@gmail.com>
2011-11-15 19:08:22 +0700

    update to 5.0.9

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -25,7 +25,7 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
-        def zkVersion = "5.0.8"
+        def zkVersion = "5.0.9"
 
         build ("net.java.dev.inflector:inflector:0.7.0")
 
