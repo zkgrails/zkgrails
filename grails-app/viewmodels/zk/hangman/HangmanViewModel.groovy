f40078946f0b98eb9671316460a5a8e193acd73c
Chanwit Kaewkasi <chanwit@gmail.com>
2011-11-25 23:14:40 +0700

    change to use the new get/set syntax

--- grails-app/viewmodels/zk/hangman/HangmanViewModel.groovy
+++ grails-app/viewmodels/zk/hangman/HangmanViewModel.groovy
@@ -8,32 +8,39 @@ class HangmanViewModel extends GrailsViewModel {
     Hangman hangman
 
     static binding = {
-        lblCount    value:"count", style:"count"
-        imgStep     src:"step"
-        lblAnswer   value:"revealedWord"
+        lblCount  value:"count", style:"count"
+        imgStep   src:"step"
+        lblAnswer value:"revealedWord"
     }
 
     @DependsOn(['hangman.win','hangman.gameOver','hangman.wrong'])
     def count = {
-        if(hangman.win)
-            [value:"you win !!", style:"color: green"]
-        else if(hangman.gameOver)
-            [value:"you win !!", style:"color: red"]
-        else
-            [value:hangman.wrong, style:"color: black"]
+        get {
+            if(hangman.win)
+                [value:"you win !!",  style:"color: green"]
+            else if(hangman.gameOver)
+                [value:"you win !!",  style:"color: red"]
+            else
+                [value:hangman.wrong, style:"color: black"]
+        }
     }
 
     @DependsOn('hangman.wrong')
-    def step = { "images/hangman/step${hangman.wrong}.gif" }
+    def step = {
+        get {
+            "images/hangman/step${hangman.wrong}.gif"
+        }
+    }
 
     @DependsOn('hangman.wrong')
     def revealedWord = {
-        def r=''
-        hangman.buffer.each {
-          r += "${it} "
+        get {
+            def r=''
+            hangman.buffer.each {
+              r += "${it} "
+            }
+            return r
         }
-        return r
     }
-
 
 }
