5d73732382b9fbd4711dbd388adcb2acacac76c2
Chanwit Kaewkasi <chanwit@gmail.com>
2011-12-16 18:47:01 +0700

    updated

--- grails-app/composers/zk/hello/HelloComposer.groovy
+++ grails-app/composers/zk/hello/HelloComposer.groovy
@@ -4,12 +4,14 @@ import org.zkoss.zk.grails.*
 
 class HelloComposer extends GrailsComposer {
 
-    def afterCompose = { wnd ->
-        binds wnd
+    def afterCompose = { wnd -> binds wnd }
+
+    static mapping = {
+        "#/world/$who"()
     }
 
-    def world() {
-        viewModel.message = "Hello World via Tag"
+    def world(who) {
+        viewModel.message = "Hello World via Tag with $who"
     }
 
     @Listen('#btnHello.onClick') showHello() {
