a5d63d313fa0f32805b3e3c08b62c136fe1d1ebb
Chanwit Kaewkasi <chanwit@gmail.com>
2012-02-07 23:31:32 +0700

    change version back to snapshot

--- ZkGrailsPlugin.groovy
+++ ZkGrailsPlugin.groovy
@@ -13,7 +13,7 @@ import org.codehaus.groovy.runtime.InvokerHelper
 
 class ZkGrailsPlugin {
     // the plugin version
-    def version = "2.0.0.M2"
+    def version = "2.0.0.BUILD-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3 > *"
     // the other plugins this plugin depends on
