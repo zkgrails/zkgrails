3281cb70c8385c882143f9949d2a8685d1048190
Chanwit Kaewkasi <chanwit@gmail.com>
2012-02-12 00:07:49 +0700

    Issue 241 : Correct package in templates

--- src/templates/artifacts/Composer.groovy
+++ src/templates/artifacts/Composer.groovy
@@ -1,4 +1,7 @@
-@artifact.package@import org.zkoss.zk.grails.*
+@artifact.package@import org.zkoss.zk.grails.composer.*
+
+import org.zkoss.zk.ui.select.annotation.Wire
+import org.zkoss.zk.ui.select.annotation.Listen
 
 class @artifact.name@ extends GrailsComposer {
 
