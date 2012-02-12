b27a2ad95381621bc44e04e4a23fc00d8aa3b0cf
Chanwit Kaewkasi <chanwit@gmail.com>
2012-02-12 21:57:28 +0700

    change version to 2.0.0.M3

--- ZkGrailsPlugin.groovy
+++ ZkGrailsPlugin.groovy
@@ -16,7 +16,7 @@ import org.zkoss.zk.grails.select.JQuery
 
 class ZkGrailsPlugin {
     // the plugin version
-    def version = "2.0.0.BUILD-SNAPSHOT"
+    def version = "2.0.0.M3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3 > *"
     // the other plugins this plugin depends on
