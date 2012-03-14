b49ae002652b540162a8d9dbf8fb59268584f63e
Chanwit Kaewkasi <chanwit@gmail.com>
2012-03-14 12:38:12 +0700

    parameterized jquery event listener

--- src/groovy/org/zkoss/zk/grails/select/JQueryEventListener.java
+++ src/groovy/org/zkoss/zk/grails/select/JQueryEventListener.java
@@ -8,13 +8,13 @@ import org.zkoss.zk.ui.event.EventListener;
 
 public class JQueryEventListener implements EventListener<Event> {
 
-    private Closure handler;
+    private Closure<?> handler;
 
-    public Closure getHandler() {
+    public Closure<?> getHandler() {
         return handler;
     }
 
-    public void setHandler(Closure handler) {
+    public void setHandler(Closure<?> handler) {
         this.handler = handler;
     }
 
